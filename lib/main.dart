3cfffba3310b1f6418fa5aa983a6ea7573c7631e
AmmarSultanDev <ammarsultan1445@gmail.com>
2024-05-29 19:58:20 +0100

    Runs well on iOS and Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,42 @@
 import 'package:flutter/material.dart';
 
+// The main function is the starting point for all our Flutter apps.
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  runApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text(
+            'I Am Rich',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  padding: EdgeInsets.all(20.0),
+                  color: Colors.white,
+                  child: Image.asset('images/logoAIM.png'),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: const Center(
+                child: Image(
+                  image: AssetImage('images/diamond.png'),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
-    );
-  }
+    ),
+  );
 }
